4d7cc5ee00b7b642072fd38552dc9a37e87a397b
rajaduttacse-fullStack <raja.dutta.cse@gmail.com>
2026-09-24 00:50:43 +0530

    Security Config  handling

--- foodiesapi/foodiesapi/src/main/java/com/food/foodiesapi/config/SecurityConfig.java
+++ foodiesapi/foodiesapi/src/main/java/com/food/foodiesapi/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/api/register", "/api/login", "/api/foods/**", "/api/orders/all", "/api/orders/status/**").permitAll()
+                        auth -> auth.requestMatchers("/api/register", "/api/login", "/api/foods", "/api/foods/**", "/api/orders/all", "/api/orders/status/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
